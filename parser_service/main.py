fb352c1eda9e9a31413205f1a99a4b9d1bbb78be
BFostek <bfmbreno@hotmail.com>
2023-12-26 21:08:30 -0300

    Integrate Redis service and enhance parser service

--- parser_service/main.py
+++ parser_service/main.py
@@ -1,10 +1,53 @@
+import json
+import logging
+import redis
+import dataclasses
+
 from parsers.ElectricityBillParser import ElectricityBillParser
 from pdfFormatter import pdf_to_array
 
+logging.basicConfig(level=logging.INFO)
+
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        return super().default(obj)
+
+
+def send_file_processed(redis_client, result):
+    redis_client.publish('file-processed', json.dumps(result, cls=ComplexEncoder))
+
+
+def process_file(file_path):
+    try:
+        parser = ElectricityBillParser(pdf_to_array(file_path))
+        result = parser.parse()
+        return result
+
+    except FileNotFoundError:
+        logging.error(f'File not found: {file_path}')
+    except Exception as e:
+        logging.error(f'Error processing file: {file_path}, {e}')
+
 
 def main():
-    print(ElectricityBillParser(pdf_to_array("faturas/3001165684-10-2023.pdf")).parse())
-    print("Hello World")
+    directory = '/app/invoices/'
+    redis_client = redis.Redis(host='redis', port=6379)
+    logging.info(f'Redis client connected: {redis_client}')
+    pubsub = redis_client.pubsub()
+    pubsub.subscribe('process-file')
+
+    for event in pubsub.listen():
+        if event['type'] == 'message':
+            data = json.loads(event['data'].decode('utf-8'))
+            file_name = data['data']['fileName']
+            logging.info(f'Received file name from process_file event: {file_name}')
+            file = process_file(file_name)
+            if not file:
+                return
+            send_file_processed(redis_client, file)
 
 
 if __name__ == "__main__":
--- parser_service/parsers/ElectricityBillParser.py
+++ parser_service/parsers/ElectricityBillParser.py
@@ -1,3 +1,5 @@
+import logging
+
 from parsers.sections.consumption_history_parser import ConsumptionHistoryParser
 from parsers.sections.invoiced_amounts_parser import InvoicedAmountsParser
 from parsers.sections.nfe_info_parser import NfeInfoParser
@@ -6,13 +8,16 @@ from parsers.sections.nfe_info_parser import NfeInfoParser
 class ElectricityBillParser:
     def __init__(self, data_array):
         self._data = data_array[0]
-        print(self._data)
         self._sections = [NfeInfoParser, ConsumptionHistoryParser, InvoicedAmountsParser]
 
     def parse(self):
-        result = {}
-        for section in self._sections:
-            item = section.parse(self._data)
-            result[item['name']] = item['content']
+        try:
+            result = {}
+            for section in self._sections:
+                item = section.parse(self._data)
+                result[item['name']] = item['content']
+            return result
+        except Exception as e:
+            logging.error(e)
+            return False
 
-        print(result)
